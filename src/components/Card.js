46b58b29e3339847bc4b138c77152715923b1fc4
Aditya Arikar <adityaarikar.26@gmail.com>
2022-11-15 15:49:22 +0530

    Card style done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,26 @@
 import React from "react";
+import "./Card.css";
+import logo from "../images/logo.png";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="card-main">
+      <div className="info-container">
+        <div className="card-center">
+          <img src={logo} alt="Logo" width={120} />
+        </div>
+        <div>
+          <text className="name">Phrom</text>
+        </div>
+        <div>
+          <text className="desc">
+            A crypto wallet reimagined for DeFi & NFTs
+          </text>
+        </div>
+      </div>
+      <div className="button-container"></div>
+    </div>
+  );
 };
 
 export default Card;
